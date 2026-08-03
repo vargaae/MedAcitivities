e480ada74893c6fe575c08dc26cfe0e0b7cb95cf
ANDRAS VARGA <vargaae@hotmail.com>
2026-08-03 15:57:23 +0200

    MED-1: Making the API Controllers thinner ->Refactor ActivitiesController to inherit from BaseApiController and update mediator usage

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,16 +8,16 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ActivitiesController(AppDbContext context, IMediator mediator) : ControllerBase
+public class ActivitiesController(AppDbContext context, IMediator mediator) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await mediator.Send(new GetActivityList.Query());
+        return await Mediator.Send(new GetActivityList.Query());
     }
 [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(string id)
     {
-        return await mediator.Send(new GetActivityDetails.Query{Id = id});
+        return await Mediator.Send(new GetActivityDetails.Query{Id = id});
     }
 }
